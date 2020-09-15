57390b695da466023881ad4143aded9a61cade10
jyoti197 <71316843+jyoti197@users.noreply.github.com>
2020-09-15 14:07:20 +0530

    Add files via upload

---
+++ src/main/java/com/oehm4/LaptopProj/App.java
@@ -0,0 +1,13 @@
+package com.oehm4.LaptopProj;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
